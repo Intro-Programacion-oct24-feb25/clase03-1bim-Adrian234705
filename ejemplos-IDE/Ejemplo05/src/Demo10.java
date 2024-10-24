b8004a74902ff880101bd566f1005a779ff702e5
Adrian Urgiles <arurgiles2@utpl.edu.ec>
2024-10-24 10:42:03 -0500

    Trabajo de Adrian

---
+++ ejemplos-IDE/Ejemplo05/src/Demo10.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author utpl
+ */
+public class Demo10 { 
+    public static void main (String[] args) {
+        int valor1 = 10; 
+        double valor2 = 3.0; 
+        int valor3 = 1; 
+        double resultado = valo1 / (valor2 + valor3); 
+        System.out.println{ resultado} ; 
+    }
+    
+}
